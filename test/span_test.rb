8ea5af58e5e72aa75d70a7adbbee771c90985993
gangadharaswamy <gangadharaswamy.h.u@gmail.com>
2019-02-19 22:35:54 +0530

    Updating test scipts to support Hash type tags in tracer

--- test/span_test.rb
+++ test/span_test.rb
@@ -10,7 +10,7 @@ class SpanTest < Minitest::Test
                                    "service",
                                    cluster: "us-west-1",
                                    shard: "primary",
-                                   custom_tags: ["custom_k", "custom_v"])
+                                   custom_tags: {"custom_k" => "custom_v"})
   end
 
   def test_ignore_active_span
@@ -19,21 +19,20 @@ class SpanTest < Minitest::Test
     scope = tracer.start_active_span('test_op')
     assert scope
     active_span = scope.span
-
     # Span created with ignore_active_span = false by default.
     child_span = tracer.start_span("child_op",
+                                   child_of: active_span,
                                    ignore_active_span: false)
     active_trace_id = active_span.trace_id.to_s
     child_trace_id = child_span.trace_id.to_s
     assert_equal active_trace_id, child_trace_id
     child_span.finish
-
     # Span created with ignore_active_span = true.
     child_span = tracer.start_span("child_op",
                                    ignore_active_span: true)
     active_trace_id = active_span.trace_id.to_s
     child_trace_id = child_span.trace_id.to_s
-    assert_not_equal active_trace_id, child_trace_id
+    refute_match active_trace_id, child_trace_id
     child_span.finish
 
     scope.close
@@ -43,19 +42,19 @@ class SpanTest < Minitest::Test
   def test_multi_valued_tags
     # test Multi-valued Tags.
     tracer = WavefrontOpentracing::Tracer.new(Reporting::ConsoleReporter.new, SpanTest.app_tags)
-    span = tracer.start_span("test_op", tags: [["key1", "val1"], ["key1", "val2"]])
+    span = tracer.start_span("test_op", tags: {"key1" => "val1", "key2" => "val2"})
     assert span
     assert span.tags
     assert span.get_tags_as_list
     assert span.get_tags_as_map
-    assert_equal 6, span.get_tags_as_map.length
+    assert_equal 7, span.get_tags_as_map.length
     assert "app", span.get_tags_as_map["application"]
     assert "service", span.get_tags_as_map["service"]
     assert "us-west-1", span.get_tags_as_map["cluster"]
     assert "primary", span.get_tags_as_map["shard"]
     assert "custom_v", span.get_tags_as_map["custom_k"]
     assert "val1", span.get_tags_as_map["key1"]
-    assert "val2", span.get_tags_as_map["key1"]
+    assert "val2", span.get_tags_as_map["key2"]
     span.finish
     tracer.close
   end
--- test/tracer_test.rb
+++ test/tracer_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 require_relative '../lib/wavefrontopentracing/tracer'
 require_relative '../lib/wavefrontopentracing/reporting/console'
-#require_relative '../util/constants'
 require_relative '../util/application_tags'
 
 class TracerTest < Minitest::Test
@@ -11,7 +10,7 @@ class TracerTest < Minitest::Test
                                    "service",
                                    cluster: "us-west-1",
                                    shard: "primary",
-                                   custom_tags: ["custom_k", "custom_v"])
+                                   custom_tags: {"custom_k" => "custom_v"})
   end
 
   def test_inject_extract
@@ -22,34 +21,35 @@ class TracerTest < Minitest::Test
     span.set_baggage_item("request_type", "mobile")
     carrier = {}
     tracer.inject(span.context, OpenTracing::FORMAT_TEXT_MAP, carrier)
-    assert_nil span.finish()
+    span.finish
     ctx = tracer.extract(OpenTracing::FORMAT_TEXT_MAP, carrier)
     assert_equal("test_customer", ctx.get_baggage_item("customer"))
     assert_equal("mobile", ctx.get_baggage_item("request_type"))
   end
 
   def test_active_span
-        # Test Active Span.
+    # Test Active Span.
     tracer = WavefrontOpentracing::Tracer.new(Reporting::ConsoleReporter.new, TracerTest.app_tags)
     span = tracer.start_span("test_op_1")
     assert span
-    assert_nil span.finish()
+    span.finish
     scope = tracer.start_active_span("test_op_2", finish_on_close: true)
     assert scope
     assert scope.span
-    assert_nil scope.close()
+    assert_nil scope.close
   end
 
   def test_global_tags
     # Test Global Tags.
-    global_tags = [["foo1", "bar1"], ["foo2", "bar2"]]
+    global_tags = {"foo1" => "bar1", "foo2" => "bar2"}
     tracer = WavefrontOpentracing::Tracer.new(Reporting::ConsoleReporter.new, TracerTest.app_tags, global_tags)
-    span = tracer.start_span("test_op" , tags: ["foo3", "bar3"])
+    span = tracer.start_span("test_op" , tags: {"foo3" => "bar3"})
     assert span
     assert span.tags
     assert span.get_tags_as_list
     assert span.get_tags_as_map
-    assert_equal 9, span.tags.length
+    assert_equal 8, span.tags.length
+    assert_equal 8, span.get_tags_as_map.length
     assert_equal "app", span.get_tags_as_map["application"]
     assert_equal "service", span.get_tags_as_map["service"]
     assert_equal "us-west-1", span.get_tags_as_map["cluster"]
@@ -58,27 +58,27 @@ class TracerTest < Minitest::Test
     assert_equal "bar1", span.get_tags_as_map["foo1"]
     assert_equal "bar2", span.get_tags_as_map["foo2"]
     assert_equal "bar3", span.get_tags_as_map["foo3"]
-    span.finish()
-    tracer.close()
+    span.finish
+    tracer.close
   end
 
   def test_global_multi_valued_tags
     # Test Global Multi-valued Tags.
-    global_tags = [["foo1", "bar1"], ["foo2", "bar2"]]
+    global_tags = {"key1" => "val1", "key1" => "val2"}
     tracer = WavefrontOpentracing::Tracer.new(Reporting::ConsoleReporter.new, TracerTest.app_tags, global_tags)
     span = tracer.start_span("test_op")
     assert span
     assert span.tags
     assert span.get_tags_as_map
-    assert_equal 7, span.get_tags_as_map.length
+    assert_equal 6, span.tags.length
+    assert_equal 6, span.get_tags_as_map.length
     assert_equal "app", span.get_tags_as_map["application"]
     assert_equal "service", span.get_tags_as_map["service"]
     assert_equal "us-west-1", span.get_tags_as_map["cluster"]
     assert_equal "primary", span.get_tags_as_map["shard"]
     assert_equal "custom_v", span.get_tags_as_map["custom_k"]
-    assert_equal "val1", span.get_tags_as_map["key1"]
     assert_equal "val2", span.get_tags_as_map["key1"]
-    span.finish()
-    tracer.close()
+    span.finish
+    tracer.close
   end
 end
